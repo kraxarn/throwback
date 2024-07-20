e716763348efa6441b0c066c39bbbed920ec4e24
kraxarn <kraxie@protonmail.com>
2024-07-20 17:37:32 +0200

    Add carousel styling

--- src/components/PlaylistCarousel.vue
+++ src/components/PlaylistCarousel.vue
@@ -2,7 +2,19 @@
 </script>
 
 <template>
+    <div class="container">
+        <slot/>
+    </div>
 </template>
 
 <style scoped>
+
+.container {
+    display: flex;
+    flex-direction: row;
+    column-gap: 1rem;
+    overflow-x: auto;
+    min-height: 200px;
+}
+
 </style>
