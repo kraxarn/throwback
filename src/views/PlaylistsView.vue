7ab1dbab55294da83eb4710c6462eef1f3b05f91
kraxarn <kraxie@protonmail.com>
2024-07-20 17:38:04 +0200

    Initial playlists view

--- src/views/PlaylistsView.vue
+++ src/views/PlaylistsView.vue
@@ -1,23 +1,79 @@
 <script setup lang="ts">
 import PlaylistTitle from "@/components/PlaylistTitle.vue"
 import PlaylistTile from "@/components/PlaylistTile.vue";
+import featured from "@/spotify/featured.json"
+import PlaylistCarousel from "@/components/PlaylistCarousel.vue";
+import {onMounted, ref} from "vue";
+import {supabase} from "@/supabaseClient";
+import {type Playlist, SpotifyApi} from "@/spotify/SpotifyApi";
+
+const spotifyApi = ref<SpotifyApi>()
+const featuredPlaylists = ref<Playlist[]>()
+const forYouPlaylists = ref<Playlist[]>()
+const userPlaylists = ref<Playlist[]>()
+
+onMounted(async () => {
+	const sessionResponse = await supabase.auth.getSession()
+	if (sessionResponse.error || !sessionResponse.data.session) {
+		alert(sessionResponse.error?.message || "Something went wrong")
+		return
+	}
+
+	const session = sessionResponse.data.session
+	const api = new SpotifyApi(session)
+	spotifyApi.value = api
+
+	api.featuredPlaylists().then(value => {
+		featuredPlaylists.value = value.playlists.items
+	})
+
+	api.search("Mix").then(value => {
+
+		forYouPlaylists.value = value.playlists.items
+			.filter(playlist => playlist.owner.id === "spotify")
+			.filter(playlist => !playlist.name.startsWith("Daily Mix"))
+			.slice(0, 20)
+	})
+
+	api.playlists().then(value => {
+		userPlaylists.value = value.items
+	})
+})
+
 </script>
 
 <template>
     <PlaylistTitle title="Featured" description="Playlists to get started"/>
 
-    <PlaylistTile/>
+    <PlaylistCarousel v-if="spotifyApi">
+        <PlaylistTile v-for="item in featured" :playlist-id="item" :spotify-api="spotifyApi"/>
+    </PlaylistCarousel>
 
     <PlaylistTitle title="Suggested" description="Playlists created by Spotify"/>
 
+    <PlaylistCarousel v-if="spotifyApi">
+        <PlaylistTile v-for="playlist in featuredPlaylists" :playlist="playlist" :spotify-api="spotifyApi"/>
+    </PlaylistCarousel>
+
     <PlaylistTitle title="For you" description="Playlists created for you"/>
+
+    <PlaylistCarousel v-if="spotifyApi">
+        <PlaylistTile v-for="playlist in forYouPlaylists" :playlist="playlist" :spotify-api="spotifyApi"/>
+    </PlaylistCarousel>
 
     <PlaylistTitle title="Your playlists" description="Your own playlists"/>
 
-    <PlaylistTitle title="Custom" description="Search for a custom playlist"/>
+    <PlaylistCarousel v-if="spotifyApi">
+        <PlaylistTile v-for="playlist in userPlaylists" :playlist="playlist" :spotify-api="spotifyApi"/>
+    </PlaylistCarousel>
+
+    <!--    <PlaylistTitle title="Custom" description="Search for a custom playlist"/>-->
 </template>
 
-<style scoped>
+<style>
 
+#app {
+    padding: 0;
+}
 
 </style>
